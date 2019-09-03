d806959999b0ba5582f72332035d15481afe5f87
Vasily93 <markovavasily@gmail.com>
2019-09-02 20:16:56 -0700

    changed api call to fetch only one quote

--- src/services/simpsonsQuotesApi.js
+++ src/services/simpsonsQuotesApi.js
@@ -1,5 +1,5 @@
 export const getQuotes = () => {
-  return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=10')
+  return fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
     .then(res => {
       if(!res.ok) throw 'unable to get quotes';
 
